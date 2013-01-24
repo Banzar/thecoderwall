782ed01c4b731cf9a0ff45b3f44f21c2b5479cc3
David Ledford <dcledford@ymail.com>
2013-01-24 14:01:20 -0500

    removed TSP

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Thecoderwall::Application.routes.draw do
 	get "videos" => "videos#index", :as => "videos"
   get "projects" => "projects#index", :as => "projects"
 	get "projects/quick_fix" => "projects#quick_fix", :as => "projects/quick_fix"
-	get "projects/trouble_shooter_plus" => "projects#trouble_shooter_plus", :as => "projects/trouble_shooter_plus"
 
 	get "posts" => "posts#index", :as => "posts"
 	get "updates" => "updates#index", :as => "updates"
