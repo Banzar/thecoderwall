6a7a5fe10422a486575feb71438c3f46d9901d63
David Ledford <dcledford@ymail.com>
2013-01-27 17:42:14 -0500

    removed password and username from code

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
-	##http_basic_authenticate_with :name => ENV["name"], :password => ENV["password"], :only => "new"
-	http_basic_authenticate_with :name => "slaitor", :password => "hello", :only => "new" and "edit"
+	http_basic_authenticate_with :name => ENV["name"], :password => ENV["password"], :only => "new" && "edit"
+
   # GET /posts
   # GET /posts.json
   def index
